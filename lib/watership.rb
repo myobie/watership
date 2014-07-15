5be7f81e52030259efc9c5c873a04e52484a33c8
myobie <me@nathanherald.com>
2014-07-15 17:30:25 +0200

    A connection per process and a channel per thread

--- lib/watership.rb
+++ lib/watership.rb
@@ -44,14 +44,14 @@ module Watership
     end
 
     def channel
-      $channel ||= connection.create_channel
+      Thread.current[:bunny_channel] ||= connection.create_channel
     rescue *CONNECTION_EXCEPTIONS => exception
       notify(exception)
-      $channel = nil
+      Thread.current[:bunny_channel] = nil
     end
 
     def connection
-      Bunny.new(@config).tap { |bunny| bunny.start }
+      $bunny_connection ||= Bunny.new(@config).tap { |bunny| bunny.start }
     end
 
     def notify(exception)
